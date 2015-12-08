4dece215bdab2f823d4eaa8a1f4e115dd9026233
Andrey A. Ugolnik <andrey@ugolnik.info>
2015-12-08 22:30:03 +0300

    C-template added.

---
+++ .vim/templates/skel.c
@@ -0,0 +1,11 @@
+/**********************************************\
+*
+*  Andrey A. Ugolnik
+*  Synesis
+*  http://www.synesis.ru
+*  andrey.ugolnik@synesis.ru
+*
+\**********************************************/
+
+@CURSOR@
+
